b84c6ccdf0009dd1df324a9717c02c235a422880
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-21 15:04:54 +0530

    Create Bean in ConfigBean class to getMovieDetails

--- src/main/java/com/niit/bej/bean/ConfigBean.java
+++ src/main/java/com/niit/bej/bean/ConfigBean.java
@@ -1,5 +1,12 @@
 package com.niit.bej.bean;
 
+import com.niit.bej.domain.Movie;
+import org.springframework.context.annotation.Bean;
+
 public class ConfigBean {
+    @Bean
+    public Movie getMovieDetails() {
+        return new Movie("Avatar-2", "Action", 2.30, 2023);
+    }
 
 }
