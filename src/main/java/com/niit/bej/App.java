78a8ccfe517c2fac4107e6eab72507bd6d201c99
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-21 15:09:25 +0530

    Create applicationContext object to get the bean of Movie class

--- src/main/java/com/niit/bej/App.java
+++ src/main/java/com/niit/bej/App.java
@@ -1,10 +1,17 @@
 package com.niit.bej;
 
+import com.niit.bej.bean.ConfigBean;
+import com.niit.bej.domain.Movie;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ConfigBean.class);
+        Movie movie1 = applicationContext.getBean(Movie.class);
+        System.out.println(movie1.getMovieName());
     }
 }
