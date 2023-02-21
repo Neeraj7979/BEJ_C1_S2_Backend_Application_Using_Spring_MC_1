0a28e8f159c375b02bee2b01efe3083242ed6f67
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-21 14:57:16 +0530

    Created constructors for fields of class Movie in domain package

--- src/main/java/com/niit/bej/domain/Movie.java
+++ src/main/java/com/niit/bej/domain/Movie.java
@@ -5,4 +5,14 @@ public class Movie {
     private String genre;
     private double movieDuration;
     private int yearOfRelease;
+
+    public Movie(String movieName, String genre, double movieDuration, int yearOfRelease) {
+        this.movieName = movieName;
+        this.genre = genre;
+        this.movieDuration = movieDuration;
+        this.yearOfRelease = yearOfRelease;
+    }
+
+    public Movie() {
+    }
 }
